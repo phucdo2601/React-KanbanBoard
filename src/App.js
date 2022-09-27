4945697f7f0a0db3577d501333403541b0061cde
phucdo2601 <ngocphucdo2601@gmail.com>
2022-09-27 07:52:25 +0700

    update ui and set start port

--- src/App.js
+++ src/App.js
@@ -4,206 +4,212 @@ import Board from "./components/boards/Board";
 import EditComp from "./components/editables/EditComp";
 import { useState } from "react";
 import { useEffect } from "react";
+import KanbanBoardPage from "./pages/KanbanBoardPage";
 
 function App() {
-  const boardInit = [
-    {
-      id: Date.now() + Math.random() * 2,
-      title: "To do",
-      cards: [
-        {
-          id: Date.now() + Math.random(),
-          title: "Card 1",
-          tasks: [],
-          labels: [
-            {
-              text: "frontend",
-              color: "blue",
-            },
-          ],
-          desc: "bdafgdg gddgdfg",
-          date: "1 Sep",
-        },
+  // const boardInit = [
+  //   {
+  //     id: Date.now() + Math.random() * 2,
+  //     title: "To do",
+  //     cards: [
+  //       {
+  //         id: Date.now() + Math.random(),
+  //         title: "Card 1",
+  //         tasks: [],
+  //         labels: [
+  //           {
+  //             text: "frontend",
+  //             color: "blue",
+  //           },
+  //         ],
+  //         desc: "bdafgdg gddgdfg",
+  //         date: "1 Sep",
+  //       },
 
-        {
-          id: Date.now() + Math.random(),
-          title: "Card 2",
-          tasks: [],
-          labels: [
-            {
-              text: "backend",
-              color: "brown",
-            },
-          ],
-          desc: "phucdn-test",
-          date: "",
-        },
-      ],
-    },
-  ];
+  //       {
+  //         id: Date.now() + Math.random(),
+  //         title: "Card 2",
+  //         tasks: [],
+  //         labels: [
+  //           {
+  //             text: "backend",
+  //             color: "brown",
+  //           },
+  //         ],
+  //         desc: "phucdn-test",
+  //         date: "",
+  //       },
+  //     ],
+  //   },
+  // ];
 
-  const [boards, setBoards] = useState(JSON.parse(localStorage.getItem('kanbanBasic')) ||[]);
+  // const [boards, setBoards] = useState(
+  //   JSON.parse(localStorage.getItem("kanbanBasic")) || []
+  // );
 
-  const [target, setTarget] = useState({
-    cid: "",
-    bid: "",
-  });
+  // const [target, setTarget] = useState({
+  //   cid: "",
+  //   bid: "",
+  // });
 
-  const addCard = (title, bid) => {
-    const card = {
-      id: Date.now() + Math.random(),
-      title,
-      labels: [],
-      tasks: [],
-      date: "",
-      desc: "",
-    };
+  // const addCard = (title, bid) => {
+  //   const card = {
+  //     id: Date.now() + Math.random(),
+  //     title,
+  //     labels: [],
+  //     tasks: [],
+  //     date: "",
+  //     desc: "",
+  //   };
 
-    const index = boards.findIndex((item) => item.id === bid);
+  //   const index = boards.findIndex((item) => item.id === bid);
 
-    if (index < 0) {
-      return;
-    }
+  //   if (index < 0) {
+  //     return;
+  //   }
 
-    const tempBoards = [...boards];
-    tempBoards[index].cards.push(card);
+  //   const tempBoards = [...boards];
+  //   tempBoards[index].cards.push(card);
 
-    setBoards(tempBoards);
-  };
+  //   setBoards(tempBoards);
+  // };
 
-  const removeCard = (cid, bid) => {
-    const bIndex = boards.findIndex((item) => item.id === bid);
+  // const removeCard = (cid, bid) => {
+  //   const bIndex = boards.findIndex((item) => item.id === bid);
 
-    if (bIndex < 0) {
-      return;
-    }
+  //   if (bIndex < 0) {
+  //     return;
+  //   }
 
-    const cIndex = boards[bIndex].cards.findIndex((item) => item.id === cid);
+  //   const cIndex = boards[bIndex].cards.findIndex((item) => item.id === cid);
 
-    if (cIndex < 0) {
-      return;
-    }
+  //   if (cIndex < 0) {
+  //     return;
+  //   }
 
-    const tempBoards = [...boards];
-    tempBoards[bIndex].cards.splice(cIndex, 1);
-    setBoards(tempBoards);
-  };
+  //   const tempBoards = [...boards];
+  //   tempBoards[bIndex].cards.splice(cIndex, 1);
+  //   setBoards(tempBoards);
+  // };
 
-  const addBoard = (title) => {
-    setBoards([
-      ...boards,
-      {
-        id: Date.now() + Math.random() * 2,
-        title,
-        cards: [],
-      },
-    ]);
-  };
+  // const addBoard = (title) => {
+  //   setBoards([
+  //     ...boards,
+  //     {
+  //       id: Date.now() + Math.random() * 2,
+  //       title,
+  //       cards: [],
+  //     },
+  //   ]);
+  // };
 
-  const removeBoard = (bid) => {
-    const tempBoards = boards.filter((item) => item.id !== bid);
-    setBoards(tempBoards);
-  };
+  // const removeBoard = (bid) => {
+  //   const tempBoards = boards.filter((item) => item.id !== bid);
+  //   setBoards(tempBoards);
+  // };
 
-  const handleDragEnter = (cid, bid) => {
-    setTarget({
-      cid,
-      bid,
-    });
-  };
+  // const handleDragEnter = (cid, bid) => {
+  //   setTarget({
+  //     cid,
+  //     bid,
+  //   });
+  // };
 
-  const handleDragEnd = (cid, bid) => {
-    let s_bIndex, s_cIndex, t_bIndex, t_cIndex;
+  // const handleDragEnd = (cid, bid) => {
+  //   let s_bIndex, s_cIndex, t_bIndex, t_cIndex;
 
-    s_bIndex = boards.findIndex((item) => item.id === bid);
-    if (s_bIndex < 0) {
-      return;
-    }
+  //   s_bIndex = boards.findIndex((item) => item.id === bid);
+  //   if (s_bIndex < 0) {
+  //     return;
+  //   }
 
-    s_cIndex = boards[s_bIndex].cards?.findIndex((item) => item.id === cid);
-    if (s_cIndex < 0) {
-      return;
-    }
+  //   s_cIndex = boards[s_bIndex].cards?.findIndex((item) => item.id === cid);
+  //   if (s_cIndex < 0) {
+  //     return;
+  //   }
 
-    t_bIndex = boards.findIndex((item) => item.id === target.bid);
-    if (t_bIndex < 0) {
-      return;
-    }
+  //   t_bIndex = boards.findIndex((item) => item.id === target.bid);
+  //   if (t_bIndex < 0) {
+  //     return;
+  //   }
 
-    t_cIndex = boards[t_bIndex].cards?.findIndex(
-      (item) => item.id === target.cid
-    );
-    if (t_cIndex < 0) {
-      return;
-    }
+  //   t_cIndex = boards[t_bIndex].cards?.findIndex(
+  //     (item) => item.id === target.cid
+  //   );
+  //   if (t_cIndex < 0) {
+  //     return;
+  //   }
 
-    const tempBoards = [...boards];
-    const tempCard = tempBoards[s_bIndex].cards[s_cIndex];
+  //   const tempBoards = [...boards];
+  //   const tempCard = tempBoards[s_bIndex].cards[s_cIndex];
 
-    tempBoards[s_bIndex].cards.splice(s_cIndex, 1);
-    tempBoards[t_bIndex].cards.splice(t_cIndex, 0, tempCard);
+  //   tempBoards[s_bIndex].cards.splice(s_cIndex, 1);
+  //   tempBoards[t_bIndex].cards.splice(t_cIndex, 0, tempCard);
 
-    setBoards(tempBoards);
-  };
+  //   setBoards(tempBoards);
+  // };
 
-  const updateCard = (cid, bid, card) => {
-    const bIndex = boards.findIndex((item) => item.id === bid);
+  // const updateCard = (cid, bid, card) => {
+  //   const bIndex = boards.findIndex((item) => item.id === bid);
 
-    if (bIndex < 0) {
-      return;
-    }
+  //   if (bIndex < 0) {
+  //     return;
+  //   }
 
-    const cIndex = boards[bIndex].cards.findIndex((item) => item.id === cid);
+  //   const cIndex = boards[bIndex].cards.findIndex((item) => item.id === cid);
 
-    if (cIndex < 0) {
-      return;
-    }
+  //   if (cIndex < 0) {
+  //     return;
+  //   }
 
+  //   const tempBoards = [...boards];
+  //   tempBoards[bIndex].cards[cIndex] = card;
+  //   setBoards(tempBoards);
+  // };
 
+  // useEffect(() => {
+  //   localStorage.setItem("kanbanBasic", JSON.stringify(boards));
+  // }, [boards]);
 
-    const tempBoards = [...boards];
-    tempBoards[bIndex].cards[cIndex] =card;
-    setBoards(tempBoards);
-  };
+  // return (
+  //   <div className="app">
+  //     <div className="app_navbar">
+  //       <h2>Kanban</h2>
+  //     </div>
 
-  useEffect(() => {
-    localStorage.setItem("kanbanBasic", JSON.stringify(boards))
-  }, [boards])
+  //     <div className="app_outer">
+  //       <div className="app_boards">
+  //         {boards.map((item) => (
+  //           <>
+  //             <Board
+  //               key={item.key}
+  //               board={item}
+  //               removeBoard={removeBoard}
+  //               addCard={addCard}
+  //               removeCard={removeCard}
+  //               handleDragEnd={handleDragEnd}
+  //               handleDragEnter={handleDragEnter}
+  //               updateCard={updateCard}
+  //             />
+  //           </>
+  //         ))}
 
+  //         <div className="app_boards_board">
+  //           <EditComp
+  //             displayClass="app_boards_board_add"
+  //             text="Add Board"
+  //             placeholder="Enter Board title"
+  //             onSubmit={(value) => addBoard(value)}
+  //           />
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
   return (
-    <div className="app">
-      <div className="app_navbar">
-        <h2>Kanban</h2>
-      </div>
-
-      <div className="app_outer">
-        <div className="app_boards">
-          {boards.map((item) => (
-            <>
-              <Board
-                key={item.key}
-                board={item}
-                removeBoard={removeBoard}
-                addCard={addCard}
-                removeCard={removeCard}
-                handleDragEnd={handleDragEnd}
-                handleDragEnter={handleDragEnter}
-                updateCard={updateCard}
-              />
-            </>
-          ))}
-
-          <div className="app_boards_board">
-            <EditComp
-              displayClass="app_boards_board_add"
-              text="Add Board"
-              placeholder="Enter Board title"
-              onSubmit={(value) => addBoard(value)}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <KanbanBoardPage />
+    </>
   );
 }
 
